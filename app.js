2ad8351f733ef1a73c514b82595aa83fb1e177aa
iamamirali <iamamirali.sh@gmail.com>
2021-08-11 11:08:03 +0430

    add content of html elements

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// whole data variable
-let weatherData;
-
-// gets data with api
 function getData() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=eslamshahr&appid=41f916b31af679c8c31a9e458d33d1a5')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=41f916b31af679c8c31a9e458d33d1a5')
     .then(response => response.json())
     .then(data => htmlSetter(data))
 }
 getData()
 
-// 
+const cityName = document.querySelector('.city-name')
+const cityStatus = document.querySelector('.city-weather-status')
+const cityTemperature = document.querySelector('.city-temperature')
+const tempIcon = document.querySelector('.city-temp-icon')
+
+function htmlSetter(data) {
+    console.log(data);
+    cityName.textContent = data.name
+    cityStatus.textContent = data.weather[0].main
+    // gets kelvin and converts it to celcius
+    cityTemperature.textContent = (data.main.temp - 273).toFixed(0)
+}
