182b61f31229fde578f2ab59e86a04354fa26092
iamamirali <iamamirali.sh@gmail.com>
2021-08-13 10:42:55 +0430

    add style to city select

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const weatherCard = document.querySelector('.weather-card')
 // hides weather card from the start
 weatherCard.style.display = 'none'
 
+let loading = false
+
 // selected city variable
 let selectedCity;
 
 function getData(city) {
+    // shows weather card
+    weatherCard.style.display = 'block'
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=41f916b31af679c8c31a9e458d33d1a5`)
     .then(response => response.json())
     .then(data => htmlSetter(data))
-    // shows weather card
-    .then(weatherCard.style.display = 'block')
+}
 
+
+function loadingSetter() {
+   if(loading) {
+    weatherCard.innerHTML = 'loading'
+   } else {
+    weatherCard.innerHTML = 'not loading'
+   }
 }
 
 const cityName = document.querySelector('.city-name')
@@ -21,8 +31,10 @@ const cityTemperature = document.querySelector('.city-temperature')
 const tempIcon = document.querySelector('.city-temp-icon')
 
 function htmlSetter(data) {
+    console.log(loading);
     cityName.innerHTML = `<img src="./Images/pin.svg" class="location-icon" alt="">${data.name}`
 
+    // city status content
     cityStatus.textContent = data.weather[0].main
     
     // gets kelvin and converts it to celcius
@@ -37,6 +49,8 @@ function tempIconSetter(data) {
         case "clouds": tempIcon.src = './Images/cloud&sun.svg'
         break;
         case "sunny": tempIcon.src = './Images/sunny.svg'
+        break;
+        case 'rain': tempIcon.src = './Images/rain.svg'
         break;
         case "cloudy": tempIcon.src = './Images/cloud.svg'
         break;
